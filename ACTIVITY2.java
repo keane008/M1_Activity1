d45587b8cb2f480d2734e60eea833ab470b0e920
Keano Kim O. Bugarin <kkobugarin@headoffice.corp.bpi.com.ph>
2025-12-03 11:56:02 +0800

    ACTIVITY COMMIT

--- ACTIVITY2.java
+++ ACTIVITY2.java
@@ -1,5 +1,23 @@
 package ph.com.bpi.hello;
+import java.util.Scanner;
 
 public class ACTIVITY2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Scanner scanner = new Scanner(System.in);
+
+  
+        System.out.print("Enter your age: ");
+        String age = scanner.nextLine();
+        
+        int ageInt = Integer.parseInt(age);
+        double ageDouble = (double) ageInt;
+        System.out.println("Your age in integer: " + ageInt);
+        System.out.println("Your age in double: " + ageDouble);
+
+        scanner.close();
+	}
+
 
 }
